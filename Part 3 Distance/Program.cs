5853ef644793ee84af7dfeb2815fea6f8d1283db
David Thomas <ThomDavi727@GoTVDSB.ca>
2020-09-30 11:50:37 -0400

    Add project files.

---
+++ Part 3 Distance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Part_3_Distance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+            int number3;
+        }
+    }
+}
