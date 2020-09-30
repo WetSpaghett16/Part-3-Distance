69463bfda5dbb7da0e948786b3fd443412b141b8
David Thomas <ThomDavi727@GoTVDSB.ca>
2020-09-30 12:15:42 -0400

    Progress made, this is safety save

--- Part 3 Distance/Program.cs
+++ Part 3 Distance/Program.cs
@@ -9,6 +9,22 @@ namespace Part_3_Distance
             int number1;
             int number2;
             int number3;
+
+            Console.WriteLine("Enter Custom Race Results in Km");
+            Console.ReadLine();
+
+            Console.WriteLine("Declare your first distance: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Declare your second distance: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Declare your third distance: ");
+            number3 = Convert.ToInt32(Console.ReadLine());
+
+
+
         }
     }
 }
